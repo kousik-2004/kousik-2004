822bf1e0b4832d79bdfa9af7824da5ab2eb79d54
kousik-2004 <gundrakousikkumarreddy2004@gmail.com>
2024-08-27 15:07:21 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,25 @@
+'''
+int
+float
+boolean
+complex
+'''
+#a=4
+#=444
+#print(type(a),type(h))
+
+
+
+#a=True
+#g=False
+#print(type(a))
+#print(True==1)
+
+#g=2.44
+#print(type(g))
+
+
+#f=4-7j
+#print(type(f))
+# t=4.57
+# print(int(t))
